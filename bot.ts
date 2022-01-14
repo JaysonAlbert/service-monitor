ea37f658807faed51c99206d8da0ddd3597a0d8f
wangjie <wangjie@htffund.com>
2022-01-14 08:58:49 +0800

    目前网页版貌似被封

--- bot.ts
+++ bot.ts
@@ -1,9 +1,17 @@
-import {Contact, log, Message, Room, ScanStatus, Wechaty} from "wechaty";
+import {
+    Contact,
+    Message,
+    ScanStatus,
+    WechatyBuilder,
+    log, Room, Sayable,
+} from 'wechaty'
+import qrcodeTerminal from 'qrcode-terminal'
 import {appAddress, ServiceHost} from "./services/address";
 import Job from "./monitor/job";
 import {JobScheduler} from "./monitor/job-scheduler";
 import {groups} from "./config/config";
 import moment from "moment";
+import * as PUPPET from "wechaty-puppet";
 
 
 const help = '1. #监控　[环境]　[系统名]\n　' +
@@ -17,7 +25,7 @@ const help = '1. #监控　[环境]　[系统名]\n　' +
 async function onLogin(user: Contact) {
     log.info('StarterBot', '%s login', user)
     const contacts: (Room | null)[] = []
-    for(const group of groups){
+    for (const group of groups) {
         let contact = await bot.Room.find({'topic': group})
         if (!contact) {
             console.log(`无法找到联系人[${group}]`)
@@ -28,7 +36,7 @@ async function onLogin(user: Contact) {
     console.log("正在添加监控器...")
     JobScheduler.schedule((v: string) => {
         if (contacts.length != 0) {
-            for(const contact of contacts){
+            for (const contact of contacts) {
                 contact?.say(v)
             }
 
@@ -38,24 +46,23 @@ async function onLogin(user: Contact) {
     })
 }
 
-const bot = new Wechaty({
-    name: "TestBot",
-    puppet: "wechaty-puppet-padlocal",
-    puppetOptions: {token: "puppet_padlocal_9ecde6faebd0491c8b15f80e1a4b08be",}
+const bot = WechatyBuilder.build({
+    name: 'service-monitor',
+    puppet: 'wechaty-puppet-wechat'
 })
 
     .on("scan", (qrcode: string, status: ScanStatus) => {
-        if (status === ScanStatus.Waiting && qrcode) {
+        if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
             const qrcodeImageUrl = [
                 'https://wechaty.js.org/qrcode/',
                 encodeURIComponent(qrcode),
             ].join('')
+            log.info('StarterBot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl)
 
-            log.info("TestBot", `onScan: ${ScanStatus[status]}(${status}) - ${qrcodeImageUrl}`);
+            qrcodeTerminal.generate(qrcode, {small: true})  // show qrcode on console
 
-            require('qrcode-terminal').generate(qrcode, {small: true})  // show qrcode on console
         } else {
-            log.info("TestBot", `onScan: ${ScanStatus[status]}(${status})`);
+            log.info('StarterBot', 'onScan: %s(%s)', ScanStatus[status], status)
         }
     })
 
@@ -70,24 +77,24 @@ const bot = new Wechaty({
             return
         }
 
-        try{
+        try {
             log.info("TestBot", `on message: ${message.toString()}`);
-        }catch (error) {
-            log.error("",error)
+        } catch (error) {
+            log.error("", error)
         }
 
-        if (message.type() !== Message.Type.Text) {
+        if (message.type() !== PUPPET.types.Message.Text) {
             return
         }
 
-        message.room()?.topic().then(topic => {
+        message.room()?.topic().then((topic: string) => {
             if (!groups.includes(topic)) {
                 return
             }
             const msg = message.text();
             if (msg.startsWith('#监控')) {
                 const l = msg.split(' ')
-                if(l.length === 3){
+                if (l.length === 3) {
                     appAddress(l[2], l[1]).then((host: ServiceHost) => {
                         const job = Job.fromHost(host)
                         console.log(`正在添加监控${JSON.stringify(job)}`)
@@ -95,18 +102,18 @@ const bot = new Wechaty({
                         JobScheduler.save()
                         message.say('监控添加成功คิดถึง')
                     }, error => {
-                        if(error&&error.msg){
+                        if (error && error.msg) {
                             message.say(error.msg)
                         }
                     }).catch(error => {
                         message.say(error)
                     })
-                }else if(l.length === 5){
-                    const job = new Job(l[2],l[1],l[3],l[4])
+                } else if (l.length === 5) {
+                    const job = new Job(l[2], l[1], l[3], l[4])
                     JobScheduler.addJob(job)
                     JobScheduler.save()
                     message.say('监控添加成功คิดถึง')
-                }else{
+                } else {
                     message.say('指令解析失败\n\n\n' + help)
                 }
 
@@ -117,14 +124,10 @@ const bot = new Wechaty({
             } else if (msg.startsWith('#list') || msg.startsWith('#列表')) {
                 message.room()?.say(`#已监控系统：\n${JobScheduler.listJobs()}`)
             }
-        }).catch(error => {
+        }).catch((error: Sayable) => {
             message.say(error)
         })
 
-    })
-
-    .on("error", (error) => {
-        log.error("TestBot", 'on error: ', error.stack);
     })
 
 export {
